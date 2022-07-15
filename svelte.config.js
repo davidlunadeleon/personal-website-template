f4d2b39a7cca00fd2ac394b2423558287a620790
David Luna de León <davidlunadeleon@gmail.com>
2022-07-14 23:04:03 -0500

    Add and config mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 import { optimizeImports } from 'carbon-preprocess-svelte';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess(), optimizeImports()],
-
+	preprocess: [preprocess(), optimizeImports(), mdsvex()],
+	extensions: ['.svelte', '.svx'],
 	kit: {
 		adapter: adapter(),
 		prerender: {
