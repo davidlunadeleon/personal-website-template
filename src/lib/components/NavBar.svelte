77e6cde7d5e94ee597a0450698e54cf862d79949
David Luna de León <davidlunadeleon@gmail.com>
2022-07-15 16:43:24 -0500

    Fixes to NavBar
    - Fix side nav moving all contents to the right when open.
    - Move SideNav inside NavBar.
    - Set expandedByDefault to false in NavBar, preventing the SideNav being
      open for a second then reloading the page.
    - Fix non existent refs in NavBar

--- src/lib/components/NavBar.svelte
+++ src/lib/components/NavBar.svelte
@@ -15,19 +15,19 @@
 	import ThemePicker from '$lib/components/ThemePicker.svelte';
 	import { t, locale, locales } from 'svelte-intl-precompile';
 
-	let isSideNavOpen = true;
+	let isSideNavOpen = false;
 	let items: { href: string; text: string }[] = [];
 	$: {
 		items = [
 			{ href: '/', text: $t('navBar.home') },
 			{ href: '/blog', text: $t('navBar.blog') },
-			{ href: '/tags', text: $t('navBar.tags') },
-			{ href: '/about', text: $t('navBar.about') }
+			{ href: '/', text: $t('navBar.tags') },
+			{ href: '/', text: $t('navBar.about') }
 		];
 	}
 </script>
 
-<Header company={config.name} bind:isSideNavOpen href="/">
+<Header company={config.name} bind:isSideNavOpen href="/" expandedByDefault={false}>
 	<svelte:fragment slot="skip-to-content">
 		<SkipToContent />
 	</svelte:fragment>
@@ -42,18 +42,17 @@
 		</HeaderNavMenu>
 		<ThemePicker />
 	</HeaderNav>
-</Header>
-
-<SideNav bind:isOpen={isSideNavOpen}>
-	<SideNavItems>
-		{#each items as item}
-			<SideNavLink {...item} />
-		{/each}
-		<SideNavMenu text={$t('navBar.language')}>
-			{#each $locales as loc}
-				<SideNavMenuItem text={$t(`langs.${loc}`)} on:click={() => ($locale = loc)} />
+	<SideNav bind:isOpen={isSideNavOpen}>
+		<SideNavItems>
+			{#each items as item}
+				<SideNavLink {...item} />
 			{/each}
-		</SideNavMenu>
-		<ThemePicker />
-	</SideNavItems>
-</SideNav>
+			<SideNavMenu text={$t('navBar.language')}>
+				{#each $locales as loc}
+					<SideNavMenuItem text={$t(`langs.${loc}`)} on:click={() => ($locale = loc)} />
+				{/each}
+			</SideNavMenu>
+			<ThemePicker />
+		</SideNavItems>
+	</SideNav>
+</Header>
