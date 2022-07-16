f72a1705b4e6cc9514c9f45237d6faef538cfc0a
David Luna de León <davidlunadeleon@gmail.com>
2022-07-16 11:30:48 -0500

    Extract post tile to its own component

---
+++ src/lib/components/PostTile.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+	import type PostMetadata from '$lib/types/PostMetadata';
+	import { Link, Button, ClickableTile } from 'carbon-components-svelte';
+	import { Calendar, Tag } from 'carbon-icons-svelte';
+	import { locale, t } from 'svelte-intl-precompile';
+	export let postMetadata: PostMetadata;
+</script>
+
+<hr />
+<ClickableTile href={`/blog/${postMetadata.slug[$locale]}`}>
+	<h2>{postMetadata.title[$locale]}</h2>
+</ClickableTile>
+<hr />
+<p>
+	<strong>
+		<Calendar />
+		{$t('blog.date')}
+		{new Date(postMetadata.publishedDate).toLocaleDateString($locale)}.
+		{#if postMetadata.publishedDate !== postMetadata.editedDate}
+			{$t('blog.editedDate')}
+			{new Date(postMetadata.editedDate).toLocaleDateString($locale)}.
+		{/if}
+		{$t('blog.by')}
+		<Link href={postMetadata.authorURL}>{postMetadata.author}</Link>
+	</strong>
+</p>
+<p>{postMetadata.summary[$locale]}</p>
+<p>
+	<strong>
+		<div class="flex-container">
+			<div>
+				<Tag />
+				{$t('blog.tags')}
+			</div>
+			{#each postMetadata.tags[$locale] as tag}
+				<div class="buttonMargin">
+					<Button href={`/tags/${tag}`} size="small">{tag}</Button>
+				</div>
+			{/each}
+		</div>
+	</strong>
+</p>
+
+<style>
+	.flex-container {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		align-items: center;
+		align-content: space-between;
+	}
+
+	.buttonMargin {
+		margin: 0.25em 0.25em;
+	}
+</style>
--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,58 +1,10 @@
 <script lang="ts">
-	import { Link, Button, ClickableTile } from 'carbon-components-svelte';
-	import { Calendar, Tag } from 'carbon-icons-svelte';
-	import { locale, t } from 'svelte-intl-precompile';
+	import PostTile from '$lib/components/PostTile.svelte';
 	import { posts } from '$lib/blog/blog';
+	import { t } from 'svelte-intl-precompile';
 </script>
 
 <h1>{$t('blog.posts')}</h1>
 {#each posts as post}
-	<hr />
-	<ClickableTile href={`/blog/${post.metadata.slug[$locale]}`}>
-		<h2>{post.metadata.title[$locale]}</h2>
-	</ClickableTile>
-	<hr />
-	<p>
-		<strong>
-			<Calendar />
-			{$t('blog.date')}
-			{new Date(post.metadata.publishedDate).toLocaleDateString($locale)}.
-			{#if post.metadata.publishedDate !== post.metadata.editedDate}
-				{$t('blog.editedDate')}
-				{new Date(post.metadata.editedDate).toLocaleDateString($locale)}.
-			{/if}
-			{$t('blog.by')}
-			<Link href={post.metadata.authorURL}>{post.metadata.author}</Link>
-		</strong>
-	</p>
-	<p>{post.metadata.summary[$locale]}</p>
-	<p>
-		<strong>
-			<div class="flex-container">
-				<div>
-					<Tag />
-					{$t('blog.tags')}
-				</div>
-				{#each post.metadata.tags[$locale] as tag}
-					<div class="buttonMargin">
-						<Button href={`/tags/${tag}`} size="small">{tag}</Button>
-					</div>
-				{/each}
-			</div>
-		</strong>
-	</p>
+	<PostTile postMetadata={post.metadata} />
 {/each}
-
-<style>
-	.flex-container {
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
-		align-items: center;
-		align-content: space-between;
-	}
-
-	.buttonMargin {
-		margin: 0.25em 0.25em;
-	}
-</style>
