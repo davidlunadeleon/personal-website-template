5c9b84d76b3d4de2c25c906825cdce93fc471181
David Luna de León <davidlunadeleon@gmail.com>
2022-07-17 09:22:08 -0500

    Set language based on URL

---
+++ src/routes/[lang=langs]/__layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { locale } from 'svelte-intl-precompile';
+	$locale = $page.params.lang;
+</script>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,15 +1,17 @@
+<script lang="ts" context="module">
+	import { addMessages, init } from 'svelte-intl-precompile';
+	import en from '$locales/en';
+	import es from '$locales/es';
+	addMessages('en', en);
+	addMessages('es', es);
+	init({ initialLocale: 'en', fallbackLocale: 'en' });
+</script>
+
 <script lang="ts">
 	import { Content, Grid, Row, Column } from 'carbon-components-svelte';
 	import 'carbon-components-svelte/css/all.css';
 	import NavBar from '$lib/components/NavBar.svelte';
 	import Footer from '$lib/components/Footer.svelte';
-	import en from '$locales/en';
-	import es from '$locales/es';
-	import { addMessages, init } from 'svelte-intl-precompile';
-
-	addMessages('en', en);
-	addMessages('es', es);
-	init({ initialLocale: 'en', fallbackLocale: 'en' });
 </script>
 
 <NavBar />
