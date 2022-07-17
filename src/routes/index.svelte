57810cbc79ce634b018bdbb38461afef6d6bddeb
David Luna de León <davidlunadeleon@gmail.com>
2022-07-17 08:49:48 -0500

    Redirect from home to the correct language page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts" context="module">
+	import { locale } from 'svelte-intl-precompile';
+	import { get } from 'svelte/store';
+
+	export async function load() {
+		return {
+			status: 302,
+			redirect: `/${get(locale).valueOf() ?? 'en'}`
+		};
+	}
+</script>
