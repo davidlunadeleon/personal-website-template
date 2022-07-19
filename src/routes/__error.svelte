554dca82e211467ad5dccf31b361e0134b13ef6f
David Luna de León <davidlunadeleon@gmail.com>
2022-07-18 19:10:00 -0500

    Add error pages

---
+++ src/routes/__error.svelte
@@ -0,0 +1,23 @@
+<script lang="ts" context="module">
+	import type { LoadEvent, LoadOutput } from '@sveltejs/kit';
+
+	export function load({ status }: LoadEvent): LoadOutput {
+		return {
+			props: {
+				status
+			}
+		};
+	}
+</script>
+
+<script lang="ts">
+	import { t } from 'svelte-intl-precompile';
+
+	export let status: number | null;
+</script>
+
+{#if status === 404}
+	<h1>{$t('error.404')}</h1>
+{:else}
+	<h1>{$t('error.unknown')}</h1>
+{/if}
